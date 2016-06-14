5aaa3a75e91b29064f5609950f7947aa65c19f85
martin <nijen1312@gmail.com>
2016-06-14 12:23:15 +0200

    auxbox added

--- Battleships.rb
+++ Battleships.rb
@@ -4,6 +4,29 @@ require "./board"
 require "./fleet"
 include Ncurses
 
+class AuxBox
+  def initialize()
+    @m_pWin = WINDOW.new(5,84,21,0)
+  end
+  def displayHints()
+    @m_pWin.wrefresh()
+    @m_pWin.box(0,0)
+    @m_pWin.mvwprintw(1,20,"Arrow keys - move around")
+    @m_pWin.mvwprintw(2,20,"R - rotate ship")
+    @m_pWin.mvwprintw(3,20,"Enter - place ship (keep distance of 1 field from other ships)")
+    @m_pWin.wrefresh()
+  end
+  def displayScore(eHP,pHP)
+    mess1="Player HP: "
+    mess2="Enemy HP: "
+    @m_pWin.getmaxyx(y=[],x=[])
+    @m_pWin.wclear()
+    @m_pWin.box(0,0)
+    @m_pWin.mvwprintw(y/2,(x-mess1.length)/2,"%s%3d",mess1,pHP)
+    @m_pWin.mvwprintw((y/2)+1,(x-mess2.length)/2,"%s%3d",mess2,eHP)
+    @m_pWin.wrefresh()
+  end
+end
 begin
   scr = Ncurses.initscr()
   Ncurses.cbreak()
@@ -13,10 +36,12 @@ begin
   playerFleet=Fleet.new(4,playerBoard)
   enemyBoard=Board.new(0,42)
   enemyFleet=Fleet.new(4,enemyBoard)
+  auxWin=AuxBox.new()
   playerFleet.initializeFleet(false)
   enemyFleet.initializeFleet(true)
   scr.refresh()
   enemyBoard.drawBoard()
+  auxWin.displayHints
   scr.refresh()
   playerBoard.drawBoard()
   playerFleet.deployFleet()
